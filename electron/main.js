5ed2dfb8a0a19543e93ec3d6abdda2d935dabe9e
Warren Gates <warrenckhan@gmail.com>
2019-08-09 11:46:21 -0400

    added packages for auto update   - update-electron-app   - electron-log
    set version to 0.0.0 for first build for testing

--- electron/main.js
+++ electron/main.js
@@ -1,12 +1,13 @@
-const {
-	app,
-	BrowserWindow
-} = require('electron');
-
+const { app, BrowserWindow } = require("electron");
+// Handles Auto Updates
+require("update-electron-app")({
+  repo: "Moikapy/moi-cast",
+  logger: require("electron-log")
+});
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-	// eslint-disable-line global-require
-	app.quit();
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
+  app.quit();
 }
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -14,54 +15,54 @@ if (require('electron-squirrel-startup')) {
 let mainWindow;
 
 const createWindow = () => {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({
-		width: 1100,
-		height: 650,
-		minWidth: 1100,
-		minHeight: 650,
-		maxWidth:1100,
-		maxHeight:650,
-		webPreferences: {
-			nodeIntegration: true
-		}
-	});
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1100,
+    height: 650,
+    minWidth: 1100,
+    minHeight: 650,
+    maxWidth: 1100,
+    maxHeight: 650,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-	// and load the index.html of the app.
-	mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+  // and load the index.html of the app.
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-	// Open the DevTools.
-	mainWindow.webContents.openDevTools();
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 
-	// Emitted when the window is closed.
-	mainWindow.on('closed', () => {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		mainWindow = null;
-	});
+  // Emitted when the window is closed.
+  mainWindow.on("closed", () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+app.on("window-all-closed", () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-app.on('activate', () => {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
-		createWindow();
-	}
+app.on("activate", () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
